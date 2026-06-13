67976587ac35621cbd12497eca500c09ab764f01
leonshimizu <Lmshimizu@gmail.com>
2026-06-13 21:13:28 +1000

    Support multiple CORS web origins

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -5,9 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+allowed_web_origins = ENV.fetch("WEB_ORIGINS", ENV.fetch("WEB_ORIGIN", "http://localhost:5173"))
+  .split(",")
+  .map(&:strip)
+  .reject(&:empty?)
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("WEB_ORIGIN", "http://localhost:5173")
+    origins(*allowed_web_origins)
 
     resource "*",
       headers: :any,
